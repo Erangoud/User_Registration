11269546f2d0ec0e274dc977080e2b670f88875f
Erangoud <egoud83@gmail.com>
2025-01-01 14:51:47 +0530

    valid first name

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import re 
 
+def validate_first_number(first_name):
+    pattern =r"^[A-Z][a-zA-Z]{2,}$"
+    match = re.match(pattern,first_name)
+    return match is not None
+
+first_name=input("enter the first name : ")
+if validate_first_number(first_name):
+    print("Valid")
+else :
+    print("invalid \nfirst letter should be CAPITAL and minimum 3 letters required")
